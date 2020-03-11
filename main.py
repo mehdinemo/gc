27d0e250c53d68399b665e845b37e648645ae455
MehdiN <nmt.1990>
2020-03-12 00:15:02 +0330

    add fit function and test

--- main.py
+++ main.py
@@ -3,6 +3,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def calculate_scores(G: nx.Graph) -> pd.DataFrame:
+    degrees = G.degree()
+
+    degrees_df = pd.DataFrame(degrees, columns=['node', 'degree'])
+    classes = pd.DataFrame(nx.get_node_attributes(G, 'label').items(), columns=['node', 'class'])
+    degrees_df = degrees_df.merge(classes, how='left', left_on='node', right_on='node')
+
+    classes = classes['class'].unique()
+
+    # create generator
+    subgraph_dic = {}
+    for i in classes:
+        sub_nodes = (
+            node
+            for node, data
+            in G.nodes(data=True)
+            if data.get('label') == i
+        )
+        subgraph = G.subgraph(sub_nodes)
+        subgraph_dic.update({i: subgraph})
+
+    sub_deg_df = pd.DataFrame()
+    for k, v in subgraph_dic.items():
+        sub_deg = v.degree()
+        sub_deg_df = sub_deg_df.append(pd.DataFrame(sub_deg, columns=['node', 'class_degree']))
+
+    degrees_df = degrees_df.merge(sub_deg_df, how='left', left_on='node', right_on='node')
+    degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
+    degrees_df.drop(['degree', 'class_degree'], axis=1, inplace=True)
+
+    return degrees_df
+
+
+def fit_nodes(scores_df: pd.DataFrame, test_edges: pd.DataFrame) -> pd.DataFrame:
+    test_nodes_list = set(test_edges[~test_edges['source'].isin(scores_df['node'])]['source'])
+    test_nodes_list.update(test_edges[~test_edges['target'].isin(scores_df['node'])]['target'])
+
+    labels = {}
+    for n in test_nodes_list:
+        neighbours = test_edges[test_edges['source'] == n]['target']
+        neighbours.append(test_edges[test_edges['target'] == n]['source'])
+
+        neighbours_df = scores_df[scores_df['node'].isin(neighbours)]
+        n_score = neighbours_df.groupby('class')['score'].sum()
+
+        duplicated_labels = n_score.duplicated(False)
+        if True in duplicated_labels.values:
+            n_label = 0
+        else:
+            n_label = n_score.idxmax()
+        labels.update({n: n_label})
+
+    labels_df = pd.DataFrame(labels.items(), columns=['node', 'label'])
+
+    return labels_df
+
+
 def main():
     edges = pd.read_csv(r'data/edges.csv')
     nodes = pd.read_csv(r'data/nodes.csv')
@@ -11,28 +68,17 @@ def main():
     G = nx.from_pandas_edgelist(edges)
     nx.set_node_attributes(G, node_dict, 'label')
 
-    labels = nx.get_node_attributes(G, 'label')
-    degrees = G.degree()
+    scores_df = calculate_scores(G)
 
-    classes = list(set(labels.values()))
-    # create generator
-    subgraph_dic = {}
-    for i in classes:
-        sub_nodes = (
-            node
-            for node, data
-            in G.nodes(data=True)
-            if data.get('label') == classes[i]
-        )
-        subgraph = G.subgraph(sub_nodes)
-        subgraph_dic.update({i: subgraph})
+    test_edges = pd.read_csv(r'data/test_edges.csv')
+    test_nodes = pd.read_csv(r'data/test_nodes.csv')
 
-    for k, v in subgraph_dic.items():
-        sub_deg = v.degree()
+    test_labels = fit_nodes(scores_df, test_edges)
 
+    test_labels = test_labels.merge(test_nodes, how='left', left_on='node', right_on='node')
 
-    nx.draw(subgraph)
-    plt.show()
+    # nx.draw(subgraph)
+    # plt.show()
 
     print('done')
 
