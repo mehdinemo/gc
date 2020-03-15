6447270e6c8df0a166117a05dcc39e1ad107a551
meti <mehdin6990@gmail.com>
2020-03-15 15:34:29 +0330

    back main to simple graph

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.metrics.pairwise import cosine_similarity
-import scipy
-
-import numpy as np
-
-from config import config
-
-from database_manager import DataBase
 
 
 def calculate_scores(G: nx.Graph) -> pd.DataFrame:
-    degrees = G.degree(weight='weight')
+    degrees = G.degree()
 
     degrees_df = pd.DataFrame(degrees, columns=['node', 'degree'])
     classes = pd.DataFrame(nx.get_node_attributes(G, 'label').items(), columns=['node', 'class'])
@@ -22,7 +12,7 @@ def calculate_scores(G: nx.Graph) -> pd.DataFrame:
 
     classes = classes['class'].unique()
 
-    # create subgragps for each class
+    # create generator
     subgraph_dic = {}
     for i in classes:
         sub_nodes = (
@@ -34,17 +24,11 @@ def calculate_scores(G: nx.Graph) -> pd.DataFrame:
         subgraph = G.subgraph(sub_nodes)
         subgraph_dic.update({i: subgraph})
 
-    # calculate degree for nodes in subgraphs
     sub_deg_df = pd.DataFrame()
     for k, v in subgraph_dic.items():
-        sub_deg = v.degree(weight='weight')
+        sub_deg = v.degree()
         sub_deg_df = sub_deg_df.append(pd.DataFrame(sub_deg, columns=['node', 'class_degree']))
 
-    # calculate scores, out of class degree for each nodes equals
-    # to degree in main graph subtract from degree in subgraph
-    # score equals to degree in subgraph subtract from degree in man graph
-    # out = degree - class_degree, score = class_degree - out
-    # score = class_degree - degree + class_degree = 2 class_degree - degree
     degrees_df = degrees_df.merge(sub_deg_df, how='left', left_on='node', right_on='node')
     degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
     degrees_df.drop(['degree', 'class_degree'], axis=1, inplace=True)
@@ -77,63 +61,26 @@ def fit_nodes(scores_df: pd.DataFrame, test_edges: pd.DataFrame) -> pd.DataFrame
 
 
 def main():
-    db = DataBase()
-    connection_string = config['connection_string']
+    edges = pd.read_csv(r'data/edges.csv')
+    nodes = pd.read_csv(r'data/nodes.csv')
+    node_dict = dict(zip(nodes['node'], nodes['class']))
 
-    with open(r'query/select_iris.sql', 'r')as file:
-        query = file.read()
-
-    data = db._select(query, connection_string)
-    data['id'] = data['id'].astype(int)
-    data['sepal_length'] = data['sepal_length'].astype(float)
-    data['sepal_width'] = data['sepal_width'].astype(float)
-    data['petal_length'] = data['petal_length'].astype(float)
-    data['petal_width'] = data['petal_width'].astype(float)
-
-    data.set_index('id', inplace=True)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']], data['class'], test_size=0.25)
-
-    node_dict = dict(zip(X_train.index, y_train))
-
-    similarities = cosine_similarity(X_train)
-    sim_mat = np.matrix(similarities)
-
-    G = nx.from_numpy_matrix(sim_mat)
-
-    mapping = dict(zip(range(0, X_train.shape[0]), X_train.index))
-    G = nx.relabel_nodes(G, mapping)
-
-    G.remove_edges_from(nx.selfloop_edges(G))
-
-    # edges = pd.read_csv(r'data/edges.csv')
-    # nodes = pd.read_csv(r'data/nodes.csv')
-    # node_dict = dict(zip(nodes['node'], nodes['class']))
-
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-    # G = nx.from_pandas_edgelist(edges, source='source', target='target', edge_attr='weight')
-
+    G = nx.from_pandas_edgelist(edges, source='source', target='target', edge_attr='weight')
     nx.set_node_attributes(G, node_dict, 'label')
+
+    bet = nx.betweenness_centrality(G, weight='weight')
 
     scores_df = calculate_scores(G)
 
-    test_train_sim = cosine_similarity(X_test, X_train)
-    test_train_sim = pd.DataFrame(test_train_sim)
-    test_train_sim.set_index(X_test.index, inplace=True)
-    test_train_sim.columns = X_train.index
+    test_edges = pd.read_csv(r'data/test_edges.csv')
+    test_nodes = pd.read_csv(r'data/test_nodes.csv')
 
+    test_labels = fit_nodes(scores_df, test_edges)
 
-    # test_edges = pd.read_csv(r'data/test_edges.csv')
-    # test_nodes = pd.read_csv(r'data/test_nodes.csv')
-    #
-    # test_labels = fit_nodes(scores_df, test_edges)
-    #
-    # test_labels = test_labels.merge(test_nodes, how='left', left_on='node', right_on='node')
-    #
-    # # nx.draw(subgraph)
-    # # plt.show()
+    test_labels = test_labels.merge(test_nodes, how='left', left_on='node', right_on='node')
+
+    # nx.draw(subgraph)
+    # plt.show()
 
     print('done')
 
--- new_iris.py
+++ new_iris.py
@@ -7,12 +7,20 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 from sklearn.model_selection import train_test_split
 import networkx as nx
+from sklearn.metrics import classification_report
 
 
 def calculate_scores(G: nx.Graph) -> pd.DataFrame:
-    degrees = G.degree(weight='weight')
+    # degree
+    # degrees = G.degree(weight='weight')
+    # degrees_df = pd.DataFrame(degrees, columns=['node', 'degree'])
 
-    degrees_df = pd.DataFrame(degrees, columns=['node', 'degree'])
+    # closeness
+    closeness = nx.closeness_centrality(G, distance='weight')
+    degrees_df = pd.DataFrame.from_dict(closeness, orient='index')
+    degrees_df.reset_index(inplace=True)
+    degrees_df.columns = ['node', 'degree']
+
     classes = pd.DataFrame(nx.get_node_attributes(G, 'label').items(), columns=['node', 'class'])
     degrees_df = degrees_df.merge(classes, how='left', left_on='node', right_on='node')
 
@@ -33,8 +41,17 @@ def calculate_scores(G: nx.Graph) -> pd.DataFrame:
     # calculate degree for nodes in subgraphs
     sub_deg_df = pd.DataFrame()
     for k, v in subgraph_dic.items():
-        sub_deg = v.degree(weight='weight')
-        sub_deg_df = sub_deg_df.append(pd.DataFrame(sub_deg, columns=['node', 'class_degree']))
+        # degree
+        # sub_deg = v.degree(weight='weight')
+        # sub_deg = pd.DataFrame(sub_deg, columns=['node', 'class_degree'])
+
+        # closeness
+        sub_deg = nx.closeness_centrality(v, distance='weight')
+        sub_deg = pd.DataFrame.from_dict(sub_deg, orient='index')
+        sub_deg.reset_index(inplace=True)
+        sub_deg.columns = ['node', 'class_degree']
+
+        sub_deg_df = sub_deg_df.append(sub_deg)
 
     # calculate scores, out of class degree for each nodes equals
     # to degree in main graph subtract from degree in subgraph
@@ -42,18 +59,56 @@ def calculate_scores(G: nx.Graph) -> pd.DataFrame:
     # out = degree - class_degree, score = class_degree - out
     # score = class_degree - degree + class_degree = 2 class_degree - degree
     degrees_df = degrees_df.merge(sub_deg_df, how='left', left_on='node', right_on='node')
-    degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
+
+    # degree
+    # degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
+
+    # closeness
+    degrees_df['score'] = degrees_df['degree'] - degrees_df['class_degree']
+    degrees_df.sort_values(by=['class', 'score'], ascending=False, inplace=True)
+
     degrees_df.drop(['degree', 'class_degree'], axis=1, inplace=True)
 
     return degrees_df
 
 
 def fit_nodes(test_train_sim, scores):
+    test_train_sim = pd.DataFrame(test_train_sim)
     scores.drop(['node'], axis=1, inplace=True)
     predict = []
     for index, row in test_train_sim.iterrows():
         scores['score_sim'] = scores['score'] * row
         n_score = scores.groupby(by=['class'])['score_sim'].sum()
+
+        duplicated_labels = n_score.duplicated(False)
+        if True in duplicated_labels.values:
+            n_label = None
+        else:
+            n_label = n_score.idxmax()
+        predict.append(n_label)
+
+    return predict
+
+
+def fit_nodes2(test_train_sim, scores, n_select):
+    test_train_sim = pd.DataFrame(test_train_sim)
+    scores.drop(['node'], axis=1, inplace=True)
+    classes = scores['class'].unique()
+    tops = pd.DataFrame(columns=scores.columns)
+    botts = pd.DataFrame(columns=scores.columns)
+    for c in classes:
+        tops = tops.append(scores[scores['class'] == c].head(n_select))
+        botts = botts.append(scores[scores['class'] == c].tail(n_select))
+
+    predict = []
+    for index, row in test_train_sim.iterrows():
+        top_ind = tops.merge(row, how='left', left_index=True, right_index=True)
+        bott_ind = botts.merge(row, how='left', left_index=True, right_index=True)
+
+        top_ind = top_ind.groupby(['class'])[index].mean()
+        bott_ind = bott_ind.groupby(['class'])[index].mean()
+
+        n_score = 2 * bott_ind - top_ind
 
         duplicated_labels = n_score.duplicated(False)
         if True in duplicated_labels.values:
@@ -84,30 +139,39 @@ def prepare_data():
                          columns=['principal component 1', 'principal component 2', 'principal component 3'])
 
     datapoints = PCAdf.values
-    # m, f = datapoints.shape
-    # k = 3
 
+    dis = euclidean_distances(datapoints)
+    sim = 1 / (1 + dis)
+    G = nx.from_numpy_matrix(sim)
+    G.remove_edges_from(nx.selfloop_edges(G))
+    node_dic = dict(zip(range(0, len(datapoints)), labels))
+    nx.set_node_attributes(G, node_dic, 'label')
+
+    scores = calculate_scores(G)
+    train_predict = fit_nodes2(sim, scores, 5)
+
+    acc = classification_report(labels, train_predict)
+    print(acc)
     # select test and train
     X_train, X_test, y_train, y_test = train_test_split(datapoints, labels, test_size=0.25)
 
     # distance and similarity
-    dis = euclidean_distances(X_train)
-    sim = 1 / (1 + dis)
+    dis_train = euclidean_distances(X_train)
+    sim_train = 1 / (1 + dis_train)
 
     # build graph for train data
-    G = nx.from_numpy_matrix(sim)
-    G.remove_edges_from(nx.selfloop_edges(G))
+    G_train = nx.from_numpy_matrix(sim_train)
+    G_train.remove_edges_from(nx.selfloop_edges(G_train))
 
-    node_dic = dict(zip(range(0, len(X_train)), y_train))
-    nx.set_node_attributes(G, node_dic, 'label')
+    train_node_dic = dict(zip(range(0, len(X_train)), y_train))
+    nx.set_node_attributes(G_train, train_node_dic, 'label')
 
-    scores_df = calculate_scores(G)
+    scores_train = calculate_scores(G_train)
 
     test_train_dis = euclidean_distances(X_test, X_train)
     test_train_sim = 1 / (1 + test_train_dis)
-    test_train_sim = pd.DataFrame(test_train_sim)
 
-    test_predict = fit_nodes(test_train_sim, scores_df)
+    test_predict = fit_nodes(test_train_sim, scores_train)
 
     print('done')
 
