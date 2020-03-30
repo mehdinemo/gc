d62c60d9ed0f70b322678e8c3544f2f62079ede4
meti <mehdin6990@gmail.com>
2020-03-30 15:43:21 +0430

    eig scores

--- tweets_classification.py
+++ tweets_classification.py
@@ -5,6 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 from sklearn.model_selection import train_test_split
 import networkx as nx
 from sklearn.metrics import classification_report
+from sklearn import preprocessing
 from sklearn import svm
 from config import config
 from database_manager import DataBase
@@ -312,20 +313,18 @@ def fit_nodes2(test_train_sim, scores, n_select=0, drop_index=False):
     return predict
 
 
-def scores_degree4(G: nx.Graph, weight: str) -> pd.DataFrame:
+def scores_degree4(G: nx.Graph, weight: str, method: str, sub_method: str) -> pd.DataFrame:
     print(f'calculate degree for graph')
-    # degrees_df = nx.eigenvector_centrality(G, weight=weight)
-    # degrees_df = pd.DataFrame.from_dict(degrees_df, orient='index')
-    # degrees_df.reset_index(inplace=True)
-    # degrees_df.columns = ['node', 'degree']
-    # degrees_df.to_csv('data/degrees_df.csv', index=False)
 
-    # degrees_df = pd.read_csv('data/degrees_df.csv')
-    # degrees_df.drop(degrees_df.columns.difference(['node', 'degree']), axis=1, inplace=True)
-
-    degrees_df = nx.degree(G, weight=weight)
-    degrees_df = pd.DataFrame(degrees_df)
-    degrees_df.columns = ['node', 'degree']
+    if method == 'eig':
+        degrees_df = nx.eigenvector_centrality(G, weight=weight)
+        degrees_df = pd.DataFrame.from_dict(degrees_df, orient='index')
+        degrees_df.reset_index(inplace=True)
+        degrees_df.columns = ['node', 'degree']
+    elif method == 'degree':
+        degrees_df = nx.degree(G, weight=weight)
+        degrees_df = pd.DataFrame(degrees_df)
+        degrees_df.columns = ['node', 'degree']
 
     print('graph degree calculated')
 
@@ -347,30 +346,41 @@ def scores_degree4(G: nx.Graph, weight: str) -> pd.DataFrame:
         subgraph_dic.update({i: subgraph})
 
     # calculate degree for nodes in subgraphs
+    min_max_scaler = preprocessing.MinMaxScaler()
     sub_deg_df = pd.DataFrame()
     for k, v in subgraph_dic.items():
         print(f'calculate eigenvector_centrality for {k}')
-        # sub_deg = nx.eigenvector_centrality(v, max_iter=200, weight=weight)
-        # sub_deg = pd.DataFrame.from_dict(sub_deg, orient='index')
-        # sub_deg.reset_index(inplace=True)
-
-        sub_deg = nx.degree(v, weight=weight)
-        sub_deg = pd.DataFrame(sub_deg)
-
-        sub_deg.columns = ['node', 'class_degree']
-        # sub_deg['class_degree'] = sub_deg['class_degree'] / sub_deg['class_degree'].sum()
+        if sub_method == 'eig':
+            sub_deg = nx.eigenvector_centrality(v, max_iter=200, weight=weight)
+            sub_deg = pd.DataFrame.from_dict(sub_deg, orient='index')
+            sub_deg.reset_index(inplace=True)
+            sub_deg.columns = ['node', 'class_degree']
+            sub_deg['class_degree'] = min_max_scaler.fit_transform(sub_deg['class_degree'])
+            # sub_deg['class_degree'] = sub_deg['class_degree'] / sub_deg['class_degree'].sum()
+        elif sub_method == 'degree':
+            sub_deg = nx.degree(v, weight=weight)
+            sub_deg = pd.DataFrame(sub_deg)
+            sub_deg.columns = ['node', 'class_degree']
 
         sub_deg_df = sub_deg_df.append(sub_deg)
 
     degrees_df = degrees_df.merge(sub_deg_df, how='left', left_on='node', right_on='node')
+
+    # degrees_df.to_csv('data/degrees_df.csv', index=False)
+
+    # degrees_df['class_degree'] = degrees_df['class_degree'] / degrees_df['class_degree'].sum()
     # degrees_df['degree'] = degrees_df['degree'] / degrees_df['degree'].sum()
     # degrees_df['class_degree'] = degrees_df['class_degree'] / degrees_df['class_degree'].sum()
 
+    degrees_df['degree'] = min_max_scaler.fit_transform(degrees_df['degree'])
+
     # degrees_df['score'] = degrees_df['class_degree'] / degrees_df['degree']
-    degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
+    degrees_df['score'] = degrees_df['class_degree'] - degrees_df['degree']
+    # degrees_df['score'] = 2 * degrees_df['class_degree'] - degrees_df['degree']
     # degrees_df.sort_values(by=['class', 'score'], ascending=False, inplace=True)
 
-    # degrees_df.to_csv('data/degrees_df.csv', index=False)
+    degrees_df.to_csv('data/degrees_df.csv', index=False)
+
     degrees_df.drop(degrees_df.columns.difference(['node', 'class', 'score']), axis=1, inplace=True)
 
     return degrees_df
@@ -422,17 +432,17 @@ def main():
 
     data_sim = jaccard_sim(data)
 
-    # clear_data_sim = sim_nodes_detector(data_sim)
+    clear_data_sim = sim_nodes_detector(data_sim)
 
     print('creating graph...')
-    G = nx.from_pandas_edgelist(data_sim, source='source', target='target', edge_attr=True)
+    G = nx.from_pandas_edgelist(clear_data_sim, source='source', target='target', edge_attr=True)
     G.remove_edges_from(nx.selfloop_edges(G))
     print(f'graph created with {len(G)} nodes and {G.number_of_edges()} edges.')
 
     node_dic = dict(zip(train['id'], train['target']))
     nx.set_node_attributes(G, node_dic, 'label')
 
-    del data, data_sim  # , clear_data_sim
+    del data, data_sim, clear_data_sim
 
     # adjacency matrix
     all_nodes = list(G.nodes)
@@ -443,10 +453,12 @@ def main():
     sim.to_csv(r'data/sim_all.csv')
 
     # print('calculate scores...')
-    # scores = scores_degree4(G, 'jaccard_sim')
+    scores = scores_degree4(G, 'jaccard_sim', 'eig', 'eig')
+
+    return
     # # scores = scores_degree(G)
-    # # scores.to_csv(r'data/scores_tweet_eig_degree.csv', index=False)
-    # # scores = pd.read_csv(r'data/scores_tweet_eig_degree.csv')
+    # scores.to_csv(r'data/scores_tweet_eig.csv', index=False)
+    # scores = pd.read_csv(r'data/scores_tweet_eig_degree.csv')
     # print('scores created')
 
     labels = nx.get_node_attributes(G, 'label')
@@ -455,6 +467,10 @@ def main():
     test_predict = pd.Series(test_predict).fillna(-1)
     acc = classification_report(list(labels.values()), list(test_predict))
     print(acc)
+
+    test_predict = test_predict.to_frame()
+    test_predict.index = sim.index
+    test_predict.to_csv('data/test_predict.csv')
 
     # # select test and train
     # data = train[train['id'].isin(all_nodes)].copy()
